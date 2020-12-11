dc992e4f7e55687d1a1b45e40a3b923591e8d988
Justin Clark <jclark@cyber.harvard.edu>
2020-12-11 18:07:11 -0500

    got jax vae working

--- jax_vae.py
+++ jax_vae.py
@@ -1,46 +1,83 @@
-from jax_nn import create_mlp, mse_loss, binary_crossentropy_loss
-from jax.experimental.stax import Relu, Sigmoid, Identity
+from functools import partial
 
-def objective(params, data, encode, decode):
-    z_params = encode(data)
+from jax import random, value_and_grad, jit
+from jax.experimental.optimizers import adam
+import jax.numpy as jnp
+import jax.scipy as jsp
+from tqdm.auto import trange
+
+def unpack_latent_params(z):
+    z_dim = z.shape[1] // 2
+    mean = z[:,:z_dim]
+    std = jnp.exp(z[:,z_dim:])
+    return mean, std
+
+def log_likelihood_data(rng_key, model, params, data, data_vari, n_latent_samples):
+    '''Expected log-likelihood of data under current model'''
+    encode, decode = model
+    enc_params, dec_params = params
+    latent_params = encode(enc_params, data)
+    mean, std = unpack_latent_params(latent_params)
+    latent_dim = mean.shape[1]
+    n_obs = data.shape[0]
+    latent_samples = random.normal(rng_key, shape=(n_latent_samples, n_obs, latent_dim)) * std + mean
+    reconstructed_data = decode(dec_params, latent_samples)
+    log_likelihood = jsp.stats.norm.logpdf(data, reconstructed_data, data_vari**0.5).sum(axis=-1)
+    assert log_likelihood.shape == (n_latent_samples, n_obs)
+    return log_likelihood, latent_samples, mean, std
+
+def log_prob_latent_under_prior(latent_samples):
+    ''' Log-probability of latent vars under N(0,1) prior '''
+    return jsp.stats.norm.logpdf(latent_samples, 0, 1).sum(axis=-1)
+
+def log_prob_latent_under_variational(latent_samples, mean, std):
+    ''' Log-probability of latent vars under variational dist '''
+    return jsp.stats.norm.logpdf(latent_samples, mean, std).sum(axis=-1)
     
-def variational_objective(params):
-            '''definition of the ELBO'''
-            encoder_weights, decoder_weights = self.unpack_weights(params)
+def general_objective(params, rng_key, model, data, data_vari, n_latent_samples=50):
+    log_likelihood, latent_samples, mean, std = log_likelihood_data(
+        rng_key, model, params, data, data_vari, n_latent_samples)
+    log_prior_prob_latent = log_prob_latent_under_prior(latent_samples)
+    log_variational_prob_latent = log_prob_latent_under_variational(latent_samples, mean, std)
+    
+    # Estimated expected value
+    elbo = (log_likelihood - log_variational_prob_latent + log_prior_prob_latent).mean()
+    return -elbo
 
-            #infer z's
-            z_params = self.encoder.forward(encoder_weights, x_train)[0]
-            
-            #unpack var parameters
-            mean, std = self.unpack_params(z_params)
-            assert std.shape == (self.z_dim, N)
-            assert mean.shape == (self.z_dim, N)
-            
-            #sample z's
-            self.random_key, subkey = random.split(self.random_key)
-            z_samples = random.normal(subkey, shape=(self.S, self.z_dim, N)) * std + mean
-            assert z_samples.shape == (self.S, self.z_dim, N)
-            
-            #predict x's
-            x = self.decoder.forward(decoder_weights, z_samples)
-            assert x.shape == (self.S, self.x_dim, N)
-            
-            #evaluate log-likelihood
-            log_likelihood = np.sum(norm.logpdf(x_train, x, self.x_var ** 0.5), axis=-2)
-            assert log_likelihood.shape == (self.S, N)
-                        
-            #evaluate sampled z's under prior
-            log_pz = np.sum(norm.logpdf(z_samples, 0.0, 1.0), axis=-2)
-            assert log_pz.shape == (self.S, N)
-            
-            #evaluate sampled z's under variational distribution
-            log_qz_given_x = np.sum(norm.logpdf(z_samples, mean, std), axis=-2)
-            assert log_qz_given_x.shape == (self.S, N)
-            
-            #compute the elbo
-            elbo = np.mean(log_likelihood - log_qz_given_x + log_pz)
-            
-            #return the negative elbo to be minimized
-            return -elbo
-            
-        return variational_objective
+def latent_dim_from_params(params):
+    # Locate the number of output layer biases
+    return len(params[-1][-2][-1])
+
+def infer(model, params, data):
+    encode, _ = model
+    enc_params, _ = params
+    return encode(enc_params, data)
+
+def generate_samples(model, params, rng_key, n=100):
+    latent_dim = latent_dim_from_params(params)
+    latent_samples = random.normal(rng_key, (n, latent_dim))
+    _, decode = model
+    _, dec_params = params
+    return decode(dec_params, latent_samples)
+
+def fit(rng_key, model, start_params, data, data_vari, step_size=1e-3, max_iter=1000):
+    '''
+    Args:
+      *model: tuple of (encode, decode)
+      *start_params: weights and biases
+      *data: array like (obs, features)
+    '''
+    opt_init, update_params, get_params = adam(step_size)
+    opt_state = opt_init(start_params)
+    history = []
+    my_elbo = jit(partial(general_objective, model=model, data=data, data_vari=data_vari))
+    min_loss_params = (1e10, None)
+    for i in trange(max_iter, smoothing=0):
+        params = get_params(opt_state)
+        rng_key, subkey = random.split(rng_key)
+        loss, grads = value_and_grad(my_elbo)(params, subkey)
+        opt_state = update_params(i, grads, opt_state)
+        if loss < min_loss_params[0]:
+            min_loss_params = (loss, params)
+        history.append((loss,))
+    return min_loss_params, history
